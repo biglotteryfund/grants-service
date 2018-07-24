fc649fcb25fa4f6b4389e251c87044a329f8c10a
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-07-24 13:19:56 +0100

    Add postcode search

--- index.js
+++ index.js
@@ -1,91 +1,115 @@
-"use strict";
-require("dotenv").config();
-const { send } = require("micro");
-const url = require("url");
-const qs = require("qs");
-const { concat } = require("lodash");
-const MongoClient = require("mongodb").MongoClient;
+'use strict';
+require('dotenv').config();
+const {send} = require('micro');
+const url = require('url');
+const qs = require('qs');
+const {concat} = require('lodash');
+const request = require('request-promise-native');
+const MongoClient = require('mongodb').MongoClient;
 
 async function connectToMongo() {
-  const client = await MongoClient.connect(process.env.MONGO_URL);
-  return client
-    .db(process.env.MONGO_DB)
-    .collection(process.env.MONGO_COLLECTION);
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    return client
+        .db(process.env.MONGO_DB)
+        .collection(process.env.MONGO_COLLECTION);
 }
 
-function buildMatchCriteria(queryParams) {
-  const match = {};
-  if (queryParams.q) {
-    match.$text = { $search: queryParams.q };
-  }
+async function lookupPostcode(postcode) {
+    return request({
+        method: 'GET',
+        url: `https://api.postcodes.io/postcodes/${postcode}`,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => JSON.parse(response));
+}
 
-  return match;
+async function buildMatchCriteria(queryParams) {
+    const match = {};
+    if (queryParams.q) {
+        match.$text = {$search: queryParams.q};
+    }
+
+    if (queryParams.postcode) {
+        try {
+            const postcodeData = await lookupPostcode(queryParams.postcode);
+            if (postcodeData && postcodeData.result) {
+                match['Recipient Org:Location:1:Geographic Code'] = {
+                    $eq: postcodeData.result.codes.admin_district
+                };
+            }
+        } catch (error) {
+            // @TODO
+        }
+    }
+
+    return match;
 }
 
 function buildFacetsCriteria(queryParams) {
-  return {
-    grantProgramme: [
-      {
-        $group: {
-          _id: "$Grant Programme:Title",
-          count: { $sum: 1 }
-        }
-      }
-    ]
-  };
+    return {
+        grantProgramme: [
+            {
+                $group: {
+                    _id: '$Grant Programme:Title',
+                    count: {$sum: 1}
+                }
+            }
+        ]
+    };
 }
 
 function buildPagination(queryParams, totalResults) {
-  const perPageCount =
-    (queryParams.limit && parseInt(queryParams.limit)) || 100;
-  const pageParam = queryParams.page && parseInt(queryParams.page);
-  const currentPage = pageParam > 1 ? pageParam : 1;
-  const skipCount = perPageCount * (currentPage - 1);
-  const totalPages = Math.ceil(totalResults / perPageCount);
+    const perPageCount =
+        (queryParams.limit && parseInt(queryParams.limit)) || 100;
+    const pageParam = queryParams.page && parseInt(queryParams.page);
+    const currentPage = pageParam > 1 ? pageParam : 1;
+    const skipCount = perPageCount * (currentPage - 1);
+    const totalPages = Math.ceil(totalResults / perPageCount);
 
-  return { currentPage, perPageCount, skipCount, totalPages };
+    return {currentPage, perPageCount, skipCount, totalPages};
 }
 
 async function fetchGrants(collection, queryParams) {
-  const matchCriteria = buildMatchCriteria(queryParams);
-  const facetsCriteria = buildFacetsCriteria(queryParams);
+    const matchCriteria = await buildMatchCriteria(queryParams);
+    const facetsCriteria = buildFacetsCriteria(queryParams);
 
-  const totalResults = await collection.find(matchCriteria).count();
+    const totalResults = await collection.find(matchCriteria).count();
 
-  const pagination = buildPagination(queryParams, totalResults);
+    const pagination = buildPagination(queryParams, totalResults);
 
-  const facets = await collection
-    .aggregate([{ $match: matchCriteria }, { $facet: facetsCriteria }])
-    .toArray();
+    const facets = await collection
+        .aggregate([{$match: matchCriteria}, {$facet: facetsCriteria}])
+        .toArray();
 
-  const results = await collection
-    .aggregate([{ $match: matchCriteria }, { $project: { _id: 0 } }])
-    .skip(pagination.skipCount)
-    .limit(pagination.perPageCount)
-    .toArray();
+    const results = await collection
+        .aggregate([{$match: matchCriteria}, {$project: {_id: 0}}])
+        .skip(pagination.skipCount)
+        .limit(pagination.perPageCount)
+        .toArray();
 
-  return {
-    results,
-    facets,
-    meta: {
-      totalResults,
-      pagination
-    }
-  };
+    return {
+        results,
+        facets,
+        meta: {
+            totalResults,
+            pagination
+        }
+    };
 }
 
 module.exports = async (req, res) => {
-  try {
-    const collection = await connectToMongo();
+    try {
+        const collection = await connectToMongo();
 
-    const queryParams = qs.parse(url.parse(req.url).search, {
-      ignoreQueryPrefix: true
-    });
+        const queryParams = qs.parse(url.parse(req.url).search, {
+            ignoreQueryPrefix: true
+        });
 
-    const results = await fetchGrants(collection, queryParams);
-    send(res, 200, results);
-  } catch (error) {
-    console.log(error);
-    send(res, 500, "Failed to connect to data store");
-  }
+        const results = await fetchGrants(collection, queryParams);
+        send(res, 200, results);
+    } catch (error) {
+        console.log(error);
+        send(res, 500, 'Failed to connect to data store');
+    }
 };
