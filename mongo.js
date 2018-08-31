a648b9a1ced9643c0c315133ee496cae4c2d4dbc
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-08-31 10:22:54 +0100

    Remove unused environments/stages and separate code into routes/modules

---
+++ mongo.js
@@ -0,0 +1,13 @@
+'use strict';
+const MongoClient = require('mongodb').MongoClient;
+
+async function connectToMongo() {
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    return client
+        .db(process.env.MONGO_DB)
+        .collection(process.env.MONGO_COLLECTION);
+}
+
+module.exports = {
+    connectToMongo
+};
