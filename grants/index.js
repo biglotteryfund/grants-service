80776c63cd7f5f59e81cf38dbaf6e04afbd4099c
David Rapson <david@davidrapson.co.uk>
2018-09-19 14:20:27 +0100

    Add a cache-control header

--- grants/index.js
+++ grants/index.js
@@ -10,6 +10,7 @@ router.route('/').get(async (req, res) => {
         const { client, collection } = await connectToMongo();
         const results = await fetchGrants(collection, req.query);
         client.close();
+        res.setHeader('cache-control', 'max-age=10,s-maxage=300');
         res.send(results);
     } catch (error) {
         res.send(error);
