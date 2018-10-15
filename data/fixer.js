496b805f878aebca7dcb1377839f841a141506a2
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-10-15 09:06:07 +0100

    Remove unused bits

--- data/fixer.js
+++ data/fixer.js
@@ -1,7 +1,6 @@
 'use strict';
 const allProgrammes = require('./allProgrammes.json');
 const dbProgrammes = require('./dbProgrammes.json');
-const stringSimilarity = require('string-similarity');
 const allTitles = allProgrammes.map(p => p.title);
 
 let totalMatches = 0;
@@ -15,10 +14,8 @@ const fixed = dbProgrammes.grantProgrammes.map(programme => {
         programme.urlPath = path;
         // console.log(`${programme._id} = ${path}`);
     } else {
-        const similarityScore = stringSimilarity.findBestMatch(programme._id, allTitles);
         const searchUrl = `https://www.google.co.uk/search?q=site%3Abiglotteryfund.org.uk+${encodeURIComponent(programme._id)}`;
         console.log(`No match for ${programme._id} - ${searchUrl}`);
-        // console.log(similarityScore.ratings.filter(s => s.rating > 0.5));
         programme.urlPath = programme._id;
     }
     return programme;
