aa73e6308b9669dbf323fd3bd2b83fce0a49042b
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-02-08 15:21:50 +0000

    Extract helper / data functions out of the cleanup script for clarity and fix inconsistencies in the data post-2019

---
+++ scripts/modules/dataTweaks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Programmes whose public-facing name differs from the internal one
+const programmesToRename = [
+    {
+        from: 'UK Accelerating Ideas',
+        to: 'UK Portfolio'
+    },
+    {
+        from: 'Fulfilling lives: HeadStart',
+        to: 'Fulfilling Lives: HeadStart'
+    }
+];
+
+// Programmes we don't display on the website
+// (eg. due to intra-funder transfers)
+const invalidProgrammeCodes = ['PFP1'];
+
+// Grants which shouldn't be listed due to incomplete status
+const invalidGrantIds = ['360G-blf-0031057998', '360G-blf-0031059356'];
+
+module.exports = {
+    programmesToRename,
+    invalidProgrammeCodes,
+    invalidGrantIds
+};
